58a83b250fa8be57a01129cf3e590086b4fc8ce1
plvkyan <kyanacademics@gmail.com>
2024-11-10 01:10:05 +0800

    Fixed 'Bill Payors' field not showing any users.

--- client/src/pages/Bills/BillForm.tsx
+++ client/src/pages/Bills/BillForm.tsx
@@ -121,7 +121,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { cn } from "@/lib/utils";
 import { format, set } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
-import MultipleSelector, { Option } from "@/components/custom/MultipleSelector";
+import MultipleSelector from "@/components/custom/MultipleSelector";
 
 
 
@@ -805,86 +805,88 @@ export const BillForm = () => {
                                             }}
                                         />
 
-                                        <FormField
-                                            control={billForm.control}
-                                            name="billPayors"
-                                            render={({ field }) => {
-                                                return (
+                                        {options.length > 0 && (
+                                            <FormField
+                                                control={billForm.control}
+                                                name="billPayors"
+                                                render={({ field }) => {
+                                                    return (
 
-                                                    <FormItem>
+                                                        <FormItem>
 
-                                                        <div className="flex gap-6 items-center justify-between pt-3" >
+                                                            <div className="flex gap-6 items-center justify-between pt-3" >
 
-                                                            <FormLabel className="w-[30%] flex gap-1 font-normal">
-                                                                Bill Payor/s
-                                                                <span className="text-destructive"> * </span>
-                                                                <TooltipProvider>
-                                                                    <Tooltip>
-                                                                        <TooltipTrigger asChild>
-                                                                            <Info className="h-4 w-4 text-muted-foreground" />
-                                                                        </TooltipTrigger>
-                                                                        <TooltipContent>
-                                                                            <p className="text-sm"> The name of the preset and the default name of bills. </p>
-                                                                        </TooltipContent>
-                                                                    </Tooltip>
-                                                                </TooltipProvider>
-                                                            </FormLabel>
+                                                                <FormLabel className="w-[30%] flex gap-1 font-normal">
+                                                                    Bill Payor/s
+                                                                    <span className="text-destructive"> * </span>
+                                                                    <TooltipProvider>
+                                                                        <Tooltip>
+                                                                            <TooltipTrigger asChild>
+                                                                                <Info className="h-4 w-4 text-muted-foreground" />
+                                                                            </TooltipTrigger>
+                                                                            <TooltipContent>
+                                                                                <p className="text-sm"> The name of the preset and the default name of bills. </p>
+                                                                            </TooltipContent>
+                                                                        </Tooltip>
+                                                                    </TooltipProvider>
+                                                                </FormLabel>
 
-                                                            <MultipleSelector
-                                                                {...field}
-                                                                defaultOptions={options}
-                                                                placeholder="Select the receiving users"
-                                                                emptyIndicator={
-                                                                    <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                                                                        No results found.
-                                                                    </p>
-                                                                }
-                                                            />
-
-                                                        </div>
-
-                                                        <div className="flex w-full gap-4 pt-2 justify-end">
-
-                                                            <Button
-                                                                className="justify-end"
-                                                                onClick={clearUsers}
-                                                                size="sm"
-                                                                type="button"
-                                                                variant="outline"
-                                                            >
-                                                                <UserX className="h-4 w-4" />
-                                                                Clear Users
-                                                            </Button>
-
-                                                            <Button
-                                                                className="justify-end"
-                                                                onClick={addUsers}
-                                                                size="sm"
-                                                                type="button"
-                                                            >
-                                                                <UserPlus className="h-4 w-4 text-primary-foreground" />
-                                                                Add All Users
-                                                            </Button>
-                                                        </div>
-
-                                                        <div className="grid grid-cols-4 items-center gap-4">
-
-                                                            <div className=""></div>
-
-                                                            <div className="col-span-3">
-
-                                                                <FormMessage />
+                                                                <MultipleSelector
+                                                                    {...field}
+                                                                    defaultOptions={options}
+                                                                    placeholder="Select the receiving users"
+                                                                    emptyIndicator={
+                                                                        <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                                                                            No results found.
+                                                                        </p>
+                                                                    }
+                                                                />
 
                                                             </div>
 
-                                                        </div>
+                                                            <div className="flex w-full gap-4 pt-2 justify-end">
 
-                                                    </FormItem>
+                                                                <Button
+                                                                    className="justify-end"
+                                                                    onClick={clearUsers}
+                                                                    size="sm"
+                                                                    type="button"
+                                                                    variant="outline"
+                                                                >
+                                                                    <UserX className="h-4 w-4" />
+                                                                    Clear Users
+                                                                </Button>
 
-                                                )
-                                            }
-                                            }
-                                        />
+                                                                <Button
+                                                                    className="justify-end"
+                                                                    onClick={addUsers}
+                                                                    size="sm"
+                                                                    type="button"
+                                                                >
+                                                                    <UserPlus className="h-4 w-4 text-primary-foreground" />
+                                                                    Add All Users
+                                                                </Button>
+                                                            </div>
+
+                                                            <div className="grid grid-cols-4 items-center gap-4">
+
+                                                                <div className=""></div>
+
+                                                                <div className="col-span-3">
+
+                                                                    <FormMessage />
+
+                                                                </div>
+
+                                                            </div>
+
+                                                        </FormItem>
+
+                                                    )
+                                                }
+                                                }
+                                            />
+                                        )}
 
 
                                         <FormField
